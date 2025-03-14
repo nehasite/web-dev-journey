20d55a91f06342d8a29210fcb7ef34e1302f681e
neha-kumari <neha.kumari80923@gmail.com>
2025-03-15 00:08:07 +0530

    add script and style file

--- project 8/script.js
+++ project 8/script.js
@@ -1,5 +1,11 @@
 let  h1Text = document.querySelector("#firsth1").
 textContent
 
-let splitedText = h1Text.split("")
-console.log(first)
+let splitedText = h1Text.split(" ")
+
+let clutter = ""
+splitedText.forEach(function(elem){
+    clutter += `<span>${elem}</span>`
+    
+})
+document.querySelector("#firsth1").innerHTML = clutter
